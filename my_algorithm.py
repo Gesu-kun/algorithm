b916e1244a0781355c11d7dfafead03c8071d328
Gesu-kun <g.tokyo.kazusa@gmail.com>
2020-06-14 02:16:25 +0900

    add ABC169 D

--- my_algorithm.py
+++ my_algorithm.py
@@ -2,33 +2,36 @@ from typing import List
 
 
 def prime_factorize(n: int) -> List[int]:
-    a = []
+    factor_list = []
     while n % 2 == 0:
-        a.append(2)
+        factor_list.append(2)
         n //= 2
     f = 3
     while f * f <= n:
         if n % f == 0:
-            a.append(f)
+            factor_list.append(f)
             n //= f
         else:
             f += 2
     if n != 1:
-        a.append(n)
-    return a
+        factor_list.append(n)
+    return factor_list
 
 
-def prime_number_enumerate(n: int) -> List[int]:
-    prime_list = []
-    search_list = list(range(2, n+1))
-    head = 2
-    while head * head <= n:
-        head = search_list[0]
-        prime_list.append(head)
-        search_list = [num for num in search_list if num % head != 0]
-    prime_list.extend(search_list)
-    return prime_list
+def prime_number_enumerate(maximum):
+    sieve = [True] * (maximum + 1)
+
+    def sieved(prime):
+        for not_prime in range(prime + prime, maximum + 1, prime):
+            sieve[not_prime] = False
+
+    sieve[0] = sieve[1] = False
+    sieved(2)
+    for x in range(3, int(maximum ** 0.5) + 1, 2):
+        if sieve[x]:
+            sieved(x)
+    return [prime for prime, is_prime in enumerate(sieve) if is_prime]
 
 
 if __name__ == "__main__":
-    print(prime_number_enumerate(1))
+    print(prime_factorize(1296))
